599593070a5a6e8a6849b14c3a2a1ea2cfeb87c7
Kirill Zhuravlev <kirjurr@gmail.com>
2016-03-10 09:23:02 +0300

    Инициализация

---
+++ bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/Users/zkv/projects/ornament/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.9.4','django-admin.py'
+__requires__ = 'Django==1.9.4'
+__import__('pkg_resources').run_script('Django==1.9.4', 'django-admin.py')
