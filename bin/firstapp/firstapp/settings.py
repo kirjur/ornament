7141456ad2040972ee742f0877b8951b8c0ed649
Kirill Zhuravlev <kirjurr@gmail.com>
2016-03-15 12:15:04 +0300

    Добавил стили, загрузку статик файлов, возможность добавлять лайки

--- bin/firstapp/firstapp/settings.py
+++ bin/firstapp/firstapp/settings.py
@@ -125,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    ('static', '/Users/zkv/projects/ornament/bin/firstapp/static'),
+    )
