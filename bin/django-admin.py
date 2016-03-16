4e93557e387f40e09beb00a4bd0067fb080965e9
Kirill Zhuravlev <kirjurr@gmail.com>
2016-03-16 16:34:33 +0300

    Удалил файлы окружения (не понятно зачем добавленные)

--- bin/django-admin.py
+++
@@ -1,4 +0,0 @@
-#!/Users/zkv/projects/ornament/bin/python
-# EASY-INSTALL-SCRIPT: 'Django==1.9.4','django-admin.py'
-__requires__ = 'Django==1.9.4'
-__import__('pkg_resources').run_script('Django==1.9.4', 'django-admin.py')
