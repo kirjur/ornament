627933c934b714129a107f8befdbcb8c5970c5a7
Kirill Zhuravlev <kirjurr@gmail.com>
2016-03-22 09:41:05 +0300

    Добавил в админку товары

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from product.models import Product, ProductType, Image
 
 # Register your models here.
+
+
+# class ProductInLine(admin.StackedInline):
+#     model = Product
+#     extra = 2
+
+
+class ProductAdmin(admin.ModelAdmin):
+    fields = ['title', 'description', 'price', 'is_available', 'is_visible', 'height', 'width', 'product_type', 'main_image']
+    inlines = [Product]
+    list_filter = ['date']
+
+admin.site.register(Product)
--- product/models.py
+++ product/models.py
@@ -18,18 +18,18 @@ class Product(models.Model):
     class Meta():
         db_table = 'products'
 
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now)
-    likes = models.IntegerField(default=0)
-    price = models.IntegerField(default=0)
-    date_delete = models.DateTimeField(null=True)
-    is_available = models.BooleanField(default=True)
-    is_visible = models.BooleanField(default=True)
-    height = models.IntegerField(default=0)
-    width = models.IntegerField(default=0)
-    product_type = models.ForeignKey(ProductType, default=1)
-    main_image = models.ForeignKey('Image', null=True, related_name="products")
+    title = models.CharField(max_length=200, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    create_date = models.DateTimeField(default=timezone.now, verbose_name="Дата создания")
+    likes = models.IntegerField(default=0, verbose_name="Количество лайков")
+    price = models.IntegerField(default=0, verbose_name="Цена")
+    date_delete = models.DateTimeField(null=True, verbose_name="Дата удаления")
+    is_available = models.BooleanField(default=True, verbose_name="Наличие")
+    is_visible = models.BooleanField(default=True, verbose_name="Видимость")
+    height = models.IntegerField(default=0, verbose_name="Высота")
+    width = models.IntegerField(default=0, verbose_name="Ширина")
+    product_type = models.ForeignKey(ProductType, default=1, verbose_name="Тип товара")
+    main_image = models.ForeignKey('Image', null=True, related_name="products", verbose_name="Главное изображение")
 
 
 class Image(models.Model):
